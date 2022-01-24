1207265c1752d0b2c06c8a2b2f044620027e88c5
Orla <orladunlop2@gmail.com>
2022-01-24 14:11:47 +0000

    Set up

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class MakersBnB < Sinatra::Base 
+  enable :sessions
+  configure :development do 
+    register Sinatra::Reloader 
+  end
+
+  get '/' do 
+    "Test!"
+    # Have sign up box in index.erb
+  end
+
+  get '/spaces' do 
+  end
+
+  get '/spaces/new' do 
+  end
+
+  run! if app_file == $0
+end
