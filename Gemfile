2ce86c7a1cd6fe78529ac00664d4ecb3a0b7778b
Alice Gray <94911337+alicegray33@users.noreply.github.com>
2022-01-25 08:23:16 +0000

    Gemfile updated with Ruby version

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@
 
 source "https://rubygems.org"
 
+ruby '3.1.0'
+
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'capybara'
--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -16,12 +16,4 @@ feature 'User Sign Up' do
     expect(page).to have_link 'Log In'  
   end
 
-  # Optional: Do we want to test for other aspects of the page?
-  # eg does the page have a title, does it have the 'Feel at home, anywhere' tagline as per the mockups?
-
-  # Optional: Test that signing up when the account already exists returns a message to user
-
-  # Optional: filling in field with invalid email and/or passwords that don't match returns a message to user.
-  # Unless input validation is handled on client side but how do we test for that?
-
 end
