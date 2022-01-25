644987b41cbdccc5c5bc46aa27697c584364194a
Orla <orladunlop2@gmail.com>
2022-01-25 09:25:53 +0000

    Created Happy Path for authenticating users

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,5 @@
 require 'pg'
+require 'bcrypt'
 
 class User 
   attr_reader :email, :id
@@ -9,11 +10,22 @@ class User
   end
 
   def self.create(email:, password:)
-    connection = PG.connect(dbname: 'makersbnb')
+    encrypted_password = BCrypt::Password.create(password)
+
+    connection = PG.connect(dbname: 'makers_bnb_test')
+    # the above will eventually be handled by a DatabaseConnection class which will know if test or not
     user = connection.exec_params(
-      "INSERT INTO users (email, password) VALUES($1, $2) RETURNING id, email;", [email, password]
+      "INSERT INTO users (email, password) VALUES($1, $2) RETURNING id, email;", [email, encrypted_password]
     )
     User.new(id: user[0]['id'], email: user[0]['email'])
+  end
+
+  def self.authenticate(email:, password:)
+    connection = PG.connect(dbname: 'makers_bnb_test')
+    result = connection.exec_params(
+      "SELECT * FROM users WHERE email = $1;", [email]
+    )
+    User.new(id: result[0]['id'], email: result[0]['email'])
   end
 end
 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,13 +1,39 @@
 require 'user'
+require 'pg'
 
 describe User do 
 
   describe '.create' do 
     it 'creates a new user' do 
       user = User.create(email: 'test@example.com', password: 'password1')
+      connection = PG.connect(dbname: 'makers_bnb_test')
 
       expect(user).to be_a User 
       expect(user.email).to eq 'test@example.com'
+
+      connection.exec("TRUNCATE users;")
+    end
+    
+    it 'the new user has a unique id' do 
+      user = User.create(email: 'idtest@example.com', password: 'password1')
+      connection = PG.connect(dbname: 'makers_bnb_test')
+      db_user = connection.exec("SELECT * FROM users WHERE email = 'idtest@example.com';")
+
+      expect(user.id).to eq db_user[0]['id']
+
+      connection.exec("TRUNCATE users;")
+    end
+  end
+
+  describe '.authenticate' do 
+    it 'returns a user given a correct username and password, if one exists' do 
+      user = User.create(email: 'test@example.com', password: 'password123')
+      authenticated_user = User.authenticate(email: 'test@example.com', password: 'password123')
+
+      expect(authenticated_user.id).to eq user.id
+
+      connection = PG.connect(dbname: 'makers_bnb_test')
+      connection.exec("TRUNCATE users;")
     end
   end
 end
