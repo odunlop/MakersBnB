9d57d03d36fb525fcab8b5fc1de793f0047ace74
Orla <orladunlop2@gmail.com>
2022-01-26 14:32:46 +0000

    Tidy capybara tests

---
+++ spec/features/add_new_space_spec.rb
@@ -0,0 +1,11 @@
+require_relative 'web_helpers'
+
+feature 'Add new space' do 
+  scenario 'User adds a new space' do 
+    log_in
+    list_panda_space
+
+    expect(page).to have_content 'Panda Space'
+    expect(page).to have_content 'Panda fun for Team Panda'
+  end
+end
---
+++ spec/features/view_spaces_spec.rb
@@ -0,0 +1,25 @@
+require_relative 'web_helpers'
+# require 'database_connection'
+require 'space'
+
+feature 'View spaces' do 
+  scenario 'View all spaces on MakersBnB' do
+    misc_spaces
+
+    log_in
+    click_button 'View Spaces'
+    expect(page).to have_content "Space1"
+    expect(page).to have_content "Space2"
+  end
+
+  # scenario 'View all my spaces' do 
+  #   misc_spaces
+  #   log_in
+  #   list_panda_space
+  #   click_button 'My Spaces'
+    
+  #   expect(page).to have_content 'Panda Space'
+  #   expect(page).not_to have_content 'Space1'
+  #   expect(page).not_to have_content 'Space2'
+  # end
+end
