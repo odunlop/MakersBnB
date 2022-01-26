596ed9be3d2ed60d5f69b35b0b863a8a37ea3f31
Tom Mayor <thomasmayor89@gmail.com>
2022-01-26 11:23:21 +0000

    added logout button with tests

--- app.rb
+++ app.rb
@@ -59,5 +59,11 @@ class MakersBnB < Sinatra::Base
     end
   end
 
+  post '/sessions/destroy' do
+    session.clear
+    flash[:notice] = 'You have signed out.'
+    redirect '/'
+  end
+
   run! if app_file == $0
 end
