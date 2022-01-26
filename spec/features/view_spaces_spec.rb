d9cc7b9e2aa13c311a6ed770111e155f5083173b
Orla <orladunlop2@gmail.com>
2022-01-26 14:46:32 +0000

    Capybara tests created for new feature

--- spec/features/view_spaces_spec.rb
+++ spec/features/view_spaces_spec.rb
@@ -12,14 +12,14 @@ feature 'View spaces' do
     expect(page).to have_content "Space2"
   end
 
-  # scenario 'View all my spaces' do 
-  #   misc_spaces
-  #   log_in
-  #   list_panda_space
-  #   click_button 'My Spaces'
+  scenario 'View all my spaces' do 
+    misc_spaces
+    log_in
+    list_panda_space
+    find('#user_spaces').click
     
-  #   expect(page).to have_content 'Panda Space'
-  #   expect(page).not_to have_content 'Space1'
-  #   expect(page).not_to have_content 'Space2'
-  # end
+    expect(page).to have_content 'Panda Space'
+    expect(page).not_to have_content 'Space1'
+    expect(page).not_to have_content 'Space2'
+  end
 end
