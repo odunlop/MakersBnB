ec7722991a3f1f9feac47d3ab8409d9c293fa856
Alice Gray <94911337+alicegray33@users.noreply.github.com>
2022-01-26 14:40:13 +0000

    bookings table added

--- db/migrations/create_db.sql
+++ db/migrations/create_db.sql
@@ -3,4 +3,6 @@ CREATE DATABASE makersbnb;
 CREATE TABLE users (id SERIAL PRIMARY KEY, email VARCHAR(60), password VARCHAR(140));
 CREATE TABLE spaces (id SERIAL PRIMARY KEY, name VARCHAR(60), description VARCHAR(140), price VARCHAR(60));
 ALTER TABLE spaces ADD creator VARCHAR(60);
+
+CREATE TABLE bookings (id SERIAL PRIMARY KEY, space_id INTEGER REFERENCES spaces (id), date DATE, confirmed BOOLEAN, user_id INTEGER REFERENCES users (id));
  
--- db/migrations/create_test_db.sql
+++ db/migrations/create_test_db.sql
@@ -4,4 +4,4 @@ CREATE TABLE users (id SERIAL PRIMARY KEY, email VARCHAR(60), password VARCHAR(1
 CREATE TABLE spaces (id SERIAL PRIMARY KEY, name VARCHAR(60), description VARCHAR(140), price VARCHAR(60));
 ALTER TABLE spaces ADD creator VARCHAR(60);
 
- 
+CREATE TABLE bookings (id SERIAL PRIMARY KEY, space_id INTEGER REFERENCES spaces (id), date DATE, confirmed BOOLEAN, user_id INTEGER REFERENCES users (id));
