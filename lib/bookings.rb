304b7be3bebc895e9a867bd03475ee94cf85f250
Tom Mayor <thomasmayor89@gmail.com>
2022-01-27 14:27:29 +0000

    .create added to space page

--- lib/bookings.rb
+++ lib/bookings.rb
@@ -3,6 +3,14 @@ require 'Date'
 
 class Bookings
 
+  def initialize(id, space_id, date, confirmed, user_id)
+    @id = id
+    @space_id = space_id
+    @date = date
+    @confirmed = confirmed
+    @user_id = user_id
+  end
+
   def self.get_availability(space_id:, date:)
     check = DatabaseConnection.query(
       "SELECT * FROM bookings WHERE space_id = $1 AND date = $2;", [space_id, date]
@@ -11,5 +19,13 @@ class Bookings
     false    
   end
 
- 
+  def self.create(space_id:, date:, confirmed:, user_id:)
+    result = DatabaseConnection.query('INSERT INTO bookings (space_id, date, confirmed, user_id) 
+    VALUES ($1, $2, $3, $4) RETURNING *;', [space_id, date, confirmed, user_id])
+    Bookings.new(result[0]['id'], result[0]['space_id'], result[0]['date'], result[0]['confirmed'], result[0]['user_id'])
+  end
+
 end
+
+ 
+
