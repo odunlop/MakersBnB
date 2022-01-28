6b6d9f75430f3d0f00a10ee441eef41b56053263
Orla <orladunlop2@gmail.com>
2022-01-28 16:11:35 +0000

    bug fix

--- lib/bookings.rb
+++ lib/bookings.rb
@@ -3,7 +3,7 @@ require 'Date'
 
 class Bookings
 
-  attr_reader :id, :space_id
+  attr_reader :id, :space_id, :user_id, :confirmed
 
   def initialize(id, space_id, date, confirmed, user_id)
     @id = id
